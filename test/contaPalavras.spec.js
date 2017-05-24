06bec1be7b7df6b1d6ee3359dbaa545ed5540987
Marcelo <marcelo.bohn@consyste.com.br>
2017-05-23 21:03:42 -0300

    novos testes

--- test/contaPalavras.spec.js
+++ test/contaPalavras.spec.js
@@ -6,5 +6,7 @@ const { contaPalavras } = require('../app/contaPalavras');
 
 lab.test('verificar se conta corretamente', (done) => {
     expect(contaPalavras("meu nome é javascript")).to.equal(4);
+    expect(contaPalavras("olá")).to.equal(1);
+    expect(contaPalavras("")).to.equal({mensagem: "Informe uma frase"});
     done();
 });
